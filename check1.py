1a1b5f0603d13a6892e62511088d51c2dea67984
Jessica Bird <jess4612@gmail.com>
2023-02-17 15:58:34 -0500

    add check1.py file to repo for first data knowledge check

---
+++ check1.py
@@ -0,0 +1,21 @@
+print('Hello world!')
+#print hello world 
+tree_species = ['Quercus virginiana', 'Malus fusca', 'Cercis canadensis']
+print(tree_species[1])
+#print a value from a list 
+trees = {'Quercus virginiana': 'Live oak', 'Malus fusca': 'Oregon crabapple',
+          'Cercis canadensis': 'Eastern redbud'}
+
+print(trees['Quercus virginiana'])
+#print a value from a dictonary
+
+tup_values = (52, 12, 101, 3, 22)
+print(max(tup_values))
+#print the max value from a tuple 
+
+
+
+
+
+
+
